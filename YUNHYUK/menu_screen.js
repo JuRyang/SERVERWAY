7abf35d913bb17cc390fbb142040d20e824a46ed
dkaylee <dkayleeee2@gmail.com>
2020-12-10 20:59:46 +0900

    .

--- YUNHYUK/menu_screen.js
+++ YUNHYUK/menu_screen.js
@@ -4,11 +4,3 @@
     choiceDiv.style.display = 'block';
     
 }*/
-
-function popup(){
-    var url ="../Serverway/choice.html"
-    var name = "popup test";
-    var option = "width=900";
-    
-    window.open(url,name,option);
-}
